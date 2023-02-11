da59f07be355f318682939cd02691c0c4cece092
RobbyGman <rguzman97@knights.ucf.edu>
2023-02-10 23:06:39 -0500

    commint

---
+++ zelda like/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    walk,
+    attack,
+    interact
+}
+public class PlayerMovement : MonoBehaviour
+{
+    public PlayerState currentState;
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector3 change;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        if(Input.GetButtonDown("attack") && currentState != PlayerState.attack)
+        {
+            StartCoroutine(AttackCo());
+        }
+        else if (currentState == PlayerState.walk)
+        {
+            UpdateAnimationAndMove();
+        }
+        
+    }
+
+    private IEnumerator AttackCo()
+    {
+        anim.SetBool("attacking", true);
+        currentState = PlayerState.attack;
+        yield return null;
+        anim.SetBool("attacking", false);
+        yield return new WaitForSeconds (.3f);
+        currentState = PlayerState.walk;
+    }
+    void UpdateAnimationAndMove()
+    {
+        if(change != Vector3.zero)
+        {
+            MoveCharacter();
+            anim.SetFloat("moveX", change.x);
+            anim.SetFloat("moveY", change.y);
+            anim.SetBool("moving", true);
+        }
+        else
+        {
+            anim.SetBool("moving", false);
+        }
+    }
+    void MoveCharacter()
+    {
+        rb.MovePosition(transform.position + change * speed * Time.deltaTime);
+    }
+}
