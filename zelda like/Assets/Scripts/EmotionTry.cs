602e52502c544ec3ccf655abea059ae614ae10c6
RobbyGman <rguzman97@knights.ucf.edu>
2023-02-12 11:12:21 -0500

    commit

---
+++ zelda like/Assets/Scripts/EmotionTry.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmotionTry : MonoBehaviour
+{
+public GameObject questionBox;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Question()
+    {
+        if (!questionBox.activeInHierarchy)
+        {
+            questionBox.SetActive(true);
+        }else{
+            questionBox.SetActive(false);
+        }
+    }}
---
+++ zelda like/Assets/Scripts/ResetToPosition.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetToPosition : MonoBehaviour
+{
+    [SerializeField] private Vector3 homePosition;
+    
+    public void ResetPosition()
+    {
+        transform.position = homePosition;
+    }
+}
