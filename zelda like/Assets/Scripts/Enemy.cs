6f4b0b0595059d7fb982024351808f5ad8065d3f
RobbyGman <rguzman97@knights.ucf.edu>
2023-02-12 20:40:47 -0500

    Commit

---
+++ zelda like/Assets/Scripts/Enemy.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyState
+{
+    idle,
+    walk,
+    attack,
+    stagger
+}
+
+public class Enemy : MonoBehaviour
+{
+    public EnemyState currentState;
+
+    [Header("Enemy Stats")]
+    public FloatValue maxHealth;
+    public float health;
+    public string enemyName;
+    public int baseAttack;
+    public float moveSpeed;
+    public Vector2 homePosition;
+
+    [Header("Death Effects")]
+    public GameObject deathEffect;
+    private float deathEffectDelay = 1f;
+    public LootTable thisLoot;
+
+    [Header("Death Signals")]
+    public SignalSender roomSignal;
+
+    private void Awake()
+    {
+        health = maxHealth.initialValue;
+    }
+
+    private void OnEnable()
+    {
+        transform.position = homePosition;
+        health = maxHealth.initialValue;
+        currentState = EnemyState.idle;
+    }
+
+    private void TakeDamage(float damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            DeathEffect();
+            MakeLoot();
+            if (roomSignal != null)
+            {
+                roomSignal.Raise();
+            }
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    private void MakeLoot()
+    {
+        if(thisLoot != null)
+        {
+            Powerup current = thisLoot.LootPowerup();
+            if(current != null)
+            {
+                Instantiate(current.gameObject, transform.position, Quaternion.identity);
+            }
+        }
+    }
+
+    private void DeathEffect()
+    {
+        if(deathEffect != null)
+        {
+            GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
+            Destroy(effect, deathEffectDelay);
+        }
+    }
+
+    public void Knock(Rigidbody2D rb, float knockTime, float damage)
+    {
+        StartCoroutine(KnockCo(rb, knockTime));
+        TakeDamage(damage);
+    }
+
+    private IEnumerator KnockCo(Rigidbody2D rb, float knockTime)
+    {
+        if (rb != null)
+        {
+            yield return new WaitForSeconds(knockTime);
+            rb.velocity = Vector2.zero;
+            currentState = EnemyState.idle;
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
---
+++ zelda like/Assets/Scripts/TreasureChest.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TreasureChest : Interactable
+{
+    [Header("Contents")]
+    public Item contents;
+    public Inventory playerInventory;
+    public bool isOpen;
+    public BoolValue storedOpen;
+
+    [Header("Signals and Dialog")]
+    public SignalSender raiseItem;
+    public GameObject dialogBox;
+    public Text dialogText;
+
+    [Header("Animation")]
+    private Animator anim;
+
+	void Start () 
+    {
+        anim = GetComponent<Animator>();
+        isOpen = storedOpen.RuntimeValue;
+        if(isOpen)
+        {
+            anim.SetBool("opened", true);
+        }
+    }
+	
+	void Update () 
+    {
+        if (Input.GetButtonDown("attack") && playerInRange)
+        {
+            if(!isOpen)
+            {
+                OpenChest();
+            }else
+            {
+                ChestAlreadyOpen();
+            }
+        }
+    }
+
+    public void OpenChest()
+    {
+        dialogBox.SetActive(true);
+        dialogText.text = contents.itemDescription;
+        playerInventory.AddItem(contents);
+        playerInventory.currentItem = contents;
+        raiseItem.Raise();
+        context.Raise();
+        isOpen = true;
+        anim.SetBool("opened", true);
+        storedOpen.RuntimeValue = isOpen;
+    }
+
+    public void ChestAlreadyOpen()
+    {
+        dialogBox.SetActive(false);
+        raiseItem.Raise();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger && !isOpen)
+        {
+            context.Raise();
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger && !isOpen)
+        {
+            context.Raise();
+            playerInRange = false;
+        }
+    }
+}
